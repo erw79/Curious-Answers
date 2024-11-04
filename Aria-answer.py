a6b575c97c486770749394778565f83560e95441
erw79 <99843247+erw79@users.noreply.github.com>
2024-11-04 16:41:48 +0000

    today

--- Aria-answer.py
+++ Aria-answer.py
@@ -1,1 +1,8 @@
+# Print an introductory message
+print("Hello!")
 
+# Ask a question and store the response in a variable
+user_input = input("What's your name? ")
+
+# Display the stored input
+print(user_input)
